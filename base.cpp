fb5a69e93ff5572b8aa663d6d39f759a3e489c08
zack256 <zack256@users.noreply.github.com>
2023-08-20 02:13:31 -0400

    cpp base

---
+++ base.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+#define mp make_pair
+#define pb push_back
+
+#define REP(i, x) for (int i = 0; i < (x); i++)
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, i, j;
+    cin >> t;
+    while (t--) {
+    }
+}
