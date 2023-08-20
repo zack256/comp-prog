cc2d210484f5c63a959053a06ef1d063ac8b8557
zack256 <zack256@users.noreply.github.com>
2023-08-20 02:34:15 -0400

    print vector and 2d

--- base.cpp
+++ base.cpp
@@ -17,6 +17,21 @@ typedef vector<ll> vl;
 
 #define REP(i, x) for (int i = 0; i < (x); i++)
 
+template<typename T>
+void print_vector (vector<T> &v, bool commas = false) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i];
+        if (commas) cout << ",";
+        cout << " ";
+    }
+    cout << endl;
+}
+
+template<typename T>
+void print_2d_matrix (vector<vector<T>> &v, bool commas = false) {
+    for (int i = 0; i < v.size(); i++) print_vector(v[i], commas);
+}
+
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
